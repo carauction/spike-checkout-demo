468f08ddef42a8d4c8e15bdfad0cd7afc90fc119
Yuki Matsukura <matsubokkuri@gmail.com>
2014-08-11 17:58:13 +0900

    improved test code

--- redis.php
+++ redis.php
@@ -1,19 +1,30 @@
 <?php
-    require 'vendor/autoload.php';
+require 'vendor/autoload.php';
+
 
 
 print_r($_ENV);
 
-    $redis = new Predis\Client(array(
-        'host' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_HOST),
-        'port' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_PORT),
-        'password' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_PASS),
-    ));
 
-    $redis->set('foo', 'bar');
-    $value = $redis->get('foo');
+print "<br>\n";
+print 'host:' . parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_HOST);
+print "<br>\n";
+print 'port:' . parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_PORT);
+print "<br>\n";
+print 'password:' . parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_PASS);
+print "<br>\n";
 
-    var_dump($value);
+
+$redis = new Predis\Client(array(
+  'host' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_HOST),
+  'port' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_PORT),
+  'password' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_PASS),
+));
+
+$redis->set('foo', 'bar');
+$value = $redis->get('foo');
+
+var_dump($value);
 
 
 
